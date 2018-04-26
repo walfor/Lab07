cc85cb6bc7044032993e248ecf83620cfd02b954
JIMMY <jimmy@MacBook-Pro-di-JIMMY.local>
2018-04-26 16:20:41 +0200

    Aggiunto progetto base Lab07

---
+++ Lab07/src/it/polito/tdp/poweroutages/db/ConnectDB.java
@@ -0,0 +1,26 @@
+package it.polito.tdp.poweroutages.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectDB {
+
+	private static final String jdbcURL = "jdbc:mysql://localhost/poweroutages?user=root";
+	private static Connection conn;
+
+	public static Connection getConnection() {
+		try {
+
+			if (conn == null || conn.isClosed()) {
+				conn = DriverManager.getConnection(jdbcURL);
+			}
+
+		} catch (SQLException e) {
+			System.err.println("Errore connessione al DB");
+			throw new RuntimeException(e);
+		}
+		return conn;
+	}
+
+}
